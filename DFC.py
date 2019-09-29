b53b02c26a1fd49cfae7078ec97233fcc7deb493
bytewicked <55121859+bytewicked@users.noreply.github.com>
2019-09-29 11:31:34 -0700

    Add files via upload

--- DFC.py
+++ DFC.py
@@ -2,62 +2,63 @@ import os
 import shutil
 
 Audio_EXT = ['.aif', '.cda', '.mid', '.midi', '.mp3', '.mpa', '.ogg',
-             '.wav', '.mwa', '.wpl'],
+             '.wav', '.mwa', '.wpl']
 
 Documents_EXT = ['.doc', '.docx', '.odt', '.pdf', '.rtf', '.tex', '.txt',
                  '.wks', '.wps', '.wpd', '.ods', '.xlr', '.xls', '.xlsx', '.key', '.odp',
                  '.pps', '.ppt', '.pptx', '.csv', '.dat', '.db', '.dbf', '.log', '.mdb',
-                 '.sav', '.sql', '.tar', '.xml', '.html'],
+                 '.sav', '.sql', '.tar', '.xml', '.html']
 
 Picture_EXT = ['.ai', '.bmp', '.gif', '.ico', '.jpeg', '.jpg', '.png',
-               '.ps', '.psd', '.svg', '.tif', '.tiff'],
+               '.ps', '.psd', '.svg', '.tif', '.tiff']
 
 Video_EXT = ['.3g2', '.3gp', '.avi', '.flv', '.h264', '.m4v', '.mkv',
-             '.mov', '.mp4', '.mpg', '.mpeg', '.rm', '.swf', '.vob', '.wmv'],
+             '.mov', '.mp4', '.mpg', '.mpeg', '.rm', '.swf', '.vob', '.wmv']
 
 Program_EXT = ['.apk', '.bat', '.bin', '.cgi', '.pl', '.com', '.exe',
-               '.gadget', '.jar', '.py', '.wsf', '.iso']
+               '.gadget', '.jar', '.py', '.wsf', '.iso','.zip']
 
 print("DFC")
 print("current Directory:  {}".format(os.getcwd()))
 print()
 
-files = os.listdir()
+files = os.listdir('.')
 
-DIRS = ['MUSIC', 'FILES', 'IMAGES', 'VIDEOS', 'PROGRAMS', 'OTHER']
-if not os.path.indir('./MUSIC'):
-    for d in DIRS:
-        os.mkdir('./{}'.format(d))
+DIRS = ['MUSIC', 'FILES', 'FOLDERS','IMAGES', 'VIDEOS', 'PROGRAMS', 'OTHER']
 
-    print("Directories Created Successfully")
+exceptions = ['./MUSIC', './FILES', './IMAGES', './VIDEOS', './PROGRAMS', './OTHER', 'DFC.py']
 
-print("Begining Sorting:...")
+for d in DIRS:
+    if not os.path.exists(d):
+        os.makedirs(d)
 
-object(f):
+print("Directories Created Successfully")
+
 for f in files:
     name, exstention = os.path.splitext(f)
-    if exstention in Audio_EXT:
-        shutil.move(f, './MUSIC/{}').format(f))
 
+    if exstention in Audio_EXT:
+        shutil.move(f, './MUSIC/{}'.format(f))
+    
     elif exstention in Documents_EXT:
-        shutil.move(f, './FILES/{}').format(f))
+        shutil.move(f, './FILES/{}'.format(f))
 
     elif exstention in Picture_EXT:
-        shutil.move(f, './IMAGES/{}').format(f))
+        shutil.move(f, './IMAGES/{}'.format(f))
 
     elif exstention in Video_EXT:
-        shutil.move(f,'./VIDEOS/{}').format(f))
+        shutil.move(f, './VIDEOS/{}'.format(f))
 
     elif exstention in Program_EXT:
-        shutil.move(f, './PROGRAMS/{}').format(f))
+        shutil.move(f, './PROGRAMS/{}'.format(f))
+
+    elif:
+        if os.path.isdir(name):
+            if name not in DIRS:
+                shutil.move(f, './FOLDERS/{}'.format(f))
 
     else:
-        if os.path.indir(name):
-            shutil.move(f, './FOLDERS/{}'.format(f))
+        if f != 'DFC.py':
+            shutil.move(f, './OTHER/{}'.format(f))
 
-        else:
-            if f !=  'DFC.py':
-                shutil.move(f, './OTHER/{}').format(f))
-
-print('Sorting Successful')
-
+print('Sorting complete')
